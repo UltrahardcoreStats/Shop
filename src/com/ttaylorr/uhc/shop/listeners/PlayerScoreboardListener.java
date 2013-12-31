ad6bcfe99fa950cac67fa7fcfd0f929ecebae016
ttaylorr <me@ttaylorr.com>
2013-12-31 15:19:57 -0500

    Update currency on every inventory click

--- src/com/ttaylorr/uhc/shop/listeners/PlayerScoreboardListener.java
+++ src/com/ttaylorr/uhc/shop/listeners/PlayerScoreboardListener.java
@@ -1,7 +1,9 @@
 package com.ttaylorr.uhc.shop.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -33,4 +35,22 @@ public class PlayerScoreboardListener implements Listener {
 	public void onPlayerKick(PlayerKickEvent event) {
 		Shop.getScoreboard().resetScores(event.getPlayer());
 	}
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (!(event.getWhoClicked()) instanceof Player) {
+            return;
+        }
+
+        Player player = (Player) event.getWhoClicked();
+
+        Score score = Shop.getObjective().getScore(player);
+        int amount = ShopUtil.getCurrencyFor(player);
+
+        if (amount != 0) {
+            score.setScore(amount);
+        }
+
+        event.getPlayer().setScoreboard(Shop.getScoreboard());
+    }
 }
